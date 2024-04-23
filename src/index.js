6db5cf4d5251cf809be99c50b0b1aefa6c625617
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-04-23 02:03:43 -0400

    Add initial Express.js setup in src/index.js

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const morgan = require("morgan")
+const { create } = require("express-handlebars")
+const path = require("path")
+
+// initializations
+const app = express()
+
+// settings
+app.set("port", process.env.PORT || 4000)
+app.set("views", path.join(__dirname, "views"))
+const hbs = create({
+  defaultLayout: "main",
+  layoutsDir: path.join(app.get("views"), "layouts"),
+  partialsDir: path.join(app.get("views"), "partials"),
+  extname: ".hbs",
+  helpers: require("./lib/handlebars"),
+})
+app.engine(".hbs", hbs.engine)
+app.set("view engine", ".hbs")
+
+// middlewares
+app.use(morgan("dev"))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// global variables
+app.use((req, res, next) => {
+  next()
+})
+
+// routes
+app.use(require("./routes"))
+app.use(require("./routes/authentication"))
+app.use("/links", require("./routes/links"))
+
+// public
+app.use(express.static(path.join(__dirname, "public")))
+
+// starting the server
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`)
+  console.log(`start http://localhost:${app.get("port")}/`)
+})
