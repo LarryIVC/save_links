f5b1fe37140ed0c2bc68916582831cecc8915e9d
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-04-26 02:28:26 -0400

    Refactor links.js to use async/await for the POST route and add error handling

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,10 +4,30 @@ const router = express.Router()
 const pool = require("../database")
 
 router.get("/add", (req, res) => {
-    res.render("links/add")
+  res.render("links/add")
 })
 
-router.post("/add", (req, res) => {
+router.post("/add", async (req, res) => {
+  //   console.log(req.body)
+  const { title, url, description } = req.body
+  const newLink = {
+    title,
+    url,
+    description,
+  }
+  try {
+    // console.log(pool)
+    await pool.query("INSERT INTO links set ?", [newLink])
     res.send("received")
+  } catch (error) {
+    console.log(error)
+  }
+  //   if (resp) {
+  //     req.flash("success", "Link saved successfully")
+  //     res.send("received")
+  //   } else {
+  //     req.flash("error", "Error saving link")
+  //     res.send("error")
+  //   }
 })
 module.exports = router
