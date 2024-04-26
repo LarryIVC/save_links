c7bbc7093b886b19ea1bcbeee6cb51b2ee913bfa
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2024-04-26 02:28:16 -0400

    Refactor database.js to use single quotes for require statements and remove unnecessary console.log statements

--- src/database.js
+++ src/database.js
@@ -1,23 +1,26 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise")
 
-const { database } = require('./keys');
+const { database } = require("./keys")
 
-const pool = mysql.createPool(database);
+const pool = mysql.createPool(database)
+// console.log(pool)
 
 pool.getConnection((err, connection) => {
-  if(err){
-    if(err.code === 'PROTOCOL_CONNECTION_LOST'){
-      console.error('DATABASE CONNECTION WAS CLOSED');
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("DATABASE CONNECTION WAS CLOSED")
     }
-    if(err.code === 'ER_CON_COUNT_ERROR'){
-      console.error('DATABASE HAS TO MANY CONNECTIONS');
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("DATABASE HAS TO MANY CONNECTIONS")
     }
-    if(err.code === 'ECONNREFUSED'){
-      console.error('DATABASE CONNECTION WAS REFUSED');
-    } 
+    if (err.code === "ECONNREFUSED") {
+      console.error("DATABASE CONNECTION WAS REFUSED")
+    }
   }
 
-  if(connection) connection.release();
-  console.log('DB is connected');
-  return;
-});
+  if (connection) connection.release()
+  console.log("DB is connected")
+  return
+})
+
+module.exports = pool 
